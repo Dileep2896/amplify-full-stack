ffe15bf08424869c68f413ffb843057a1bc98a3b
Dileep2896 <dileepkumar2896@gmail.com>
2024-07-11 14:51:18 -0400

    added authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
-function App() {
-  return <>HELLO</>;
-}
+import logo from "/vite.svg";
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
 
-export default App;
+// eslint-disable-next-line react-refresh/only-export-components
+const App = ({ signOut }) => {
+  return (
+    <View>
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
+  );
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export default withAuthenticator(App);
